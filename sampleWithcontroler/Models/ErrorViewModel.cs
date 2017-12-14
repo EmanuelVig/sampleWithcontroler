6c9504758861257d3346f32d203d3a0daa1acc84
Emanuel Vig <ic15b066@technikum-wien.at>
2017-12-14 14:20:59 +0100

    Add project files.

---
+++ sampleWithcontroler/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sampleWithcontroler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
